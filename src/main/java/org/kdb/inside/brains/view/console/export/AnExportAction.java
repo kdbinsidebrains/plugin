741cbf29d08f5c6f41188920e6ef801725e6bab3
smklimenko <smklimenko@gmail.com>
2021-12-26 13:14:35 +0000

    ## [0.16.0]
    ### Changed
    
    - Excel exporting format changed to xlsx (Excel 2007) instead of xls (Excel 95)
      - 1,048,576 rows instead of 65,536 and 16,384 cols instead of 256
      - Streams worksheet is used to reduce memory footprint
    
    - Export actions run as a background task to reduce UI freeze

--- src/main/java/org/kdb/inside/brains/view/console/export/AnExportAction.java
+++ src/main/java/org/kdb/inside/brains/view/console/export/AnExportAction.java
@@ -2,15 +2,22 @@ package org.kdb.inside.brains.view.console.export;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.PerformInBackgroundOption;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.ui.tabs.JBTabs;
+import com.intellij.ui.tabs.TabInfo;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.kdb.inside.brains.view.console.TableResultView;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
-public abstract class AnExportAction extends AnAction {
+public abstract class AnExportAction<Config> extends AnAction {
     private final ExportingType type;
     private final TableResultView resultView;
 
@@ -29,6 +36,16 @@ public abstract class AnExportAction extends AnAction {
     }
 
     @Override
+    public void update(@NotNull AnActionEvent e) {
+        final TableResultView view = getTableResultView(e);
+        if (view == null) {
+            e.getPresentation().setEnabled(false);
+        } else {
+            e.getPresentation().setEnabled(type.hasExportingData(view));
+        }
+    }
+
+    @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         final Project project = e.getProject();
         final TableResultView view = getTableResultView(e);
@@ -43,21 +60,49 @@ public abstract class AnExportAction extends AnAction {
             return;
         }
 
+        final Config config;
         try {
-            performAction(project, view, type);
+            config = getExportConfig(project, view);
+            if (config == null) {
+                return;
+            }
         } catch (Exception ex) {
             Messages.showErrorDialog(project, ex.getMessage(), "Data Can't Be Exported");
+            return;
         }
+
+        final String title = "Exporting " + getExportingName(view);
+        new Task.Backgroundable(project, title, isCancelable(), PerformInBackgroundOption.DEAF) {
+            @Override
+            public void run(@NotNull ProgressIndicator indicator) {
+                try {
+                    exportResultView(project, type, view, config, indicator);
+                } catch (Exception ex) {
+                    Messages.showErrorDialog(project, ex.getMessage(), "Data Can't Be Exported");
+                }
+            }
+        }.queue();
     }
 
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-        final TableResultView view = getTableResultView(e);
-        if (view == null) {
-            e.getPresentation().setEnabled(false);
-        } else {
-            e.getPresentation().setEnabled(type.hasExportingData(view));
+    protected boolean isCancelable() {
+        return true;
+    }
+
+    protected abstract Config getExportConfig(Project project, TableResultView view) throws IOException;
+
+    protected abstract void exportResultView(Project project, ExportingType type, TableResultView view, Config config, @NotNull ProgressIndicator indicator) throws Exception;
+
+    private String getExportingName(TableResultView view) {
+        final Container parent = view.getParent();
+        if (parent instanceof JBTabs) {
+            final JBTabs jbTabs = (JBTabs) parent;
+            for (TabInfo info : jbTabs.getTabs()) {
+                if (info.getObject() == view) {
+                    return info.getText();
+                }
+            }
         }
+        return "Table Result";
     }
 
     @Nullable
@@ -67,6 +112,4 @@ public abstract class AnExportAction extends AnAction {
         }
         return TableResultView.DATA_KEY.getData(e.getDataContext());
     }
-
-    protected abstract void performAction(Project project, TableResultView view, ExportingType type) throws Exception;
 }
